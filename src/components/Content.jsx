60a20ebe20e2cdfb6012e5d88cb6892c8880891d
Ewelina Fiedorowicz <ewelinaformella@gmail.com>
2020-01-06 16:09:42 +0100

    reservation handling added

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import PropType from 'prop-types';
 import CircularProgress from "@material-ui/core/CircularProgress";
 
@@ -9,73 +9,76 @@ import Reservation from "./contentViews/Reservation";
 
 class Content extends Component {
 
-    contentWrapper = {
-        height: '100%',
-        width: '95%',
-        padding: '20px 0',
-        zIndex: 2,
-        boxSizing: 'border-box',
-        overflowY: 'auto'
-    };
+  contentWrapper = {
+    height: '100%',
+    width: '95%',
+    padding: '20px 0',
+    zIndex: 2,
+    boxSizing: 'border-box',
+    overflowY: 'auto'
+  };
 
-    render() {
-        return (<div className={'maxHeight'} style={this.contentWrapper}>
+  render() {
+    return (<div className={'maxHeight'} style={this.contentWrapper}>
 
-                  {!this.props.pageView ?
-                   <div style={{margin: '20px auto', textAlign: 'center'}}>
-                     <CircularProgress size={100} color="secondary"/>
-                   </div>
-                   : null}
+      {!this.props.pageView ?
+        <div style={{ margin: '20px auto', textAlign: 'center' }}>
+          <CircularProgress size={100} color="secondary" />
+        </div>
+        : null}
 
-                  {this.props.pageView & 0b0001 ?
-                   <MovieList
-                     city={this.props.city}
-                     movies={this.props.movies}
-                     onSelect={this.props.onMovieSelect}
-                   /> : null}
+      {this.props.pageView & 0b0001 ?
+        <MovieList
+          city={this.props.city}
+          movies={this.props.movies}
+          onSelect={this.props.onMovieSelect}
+        /> : null}
 
-                  {this.props.pageView & 0b0010 ?
-                   <ShowtimeList
-                     showtimes={this.props.showtimes}
-                     showName={this.props.selectedMovie}
-                     poster={this.props.poster}
-                     onSelect={this.props.onShowtimeSelect}
-                   /> : null}
+      {this.props.pageView & 0b0010 ?
+        <ShowtimeList
+          showtimes={this.props.showtimes}
+          showName={this.props.selectedMovie}
+          poster={this.props.poster}
+          onSelect={this.props.onShowtimeSelect}
+        /> : null}
 
-                  {this.props.pageView & 0b0100 ?
-                   <Sits
-                     sits={this.props.sits}
-                     onSelect={this.props.onSitSelect}
-                   /> : null}
+      {this.props.pageView & 0b0100 ?
+        <Sits
+          sits={this.props.sits}
+          onSelect={this.props.onSitSelect}
+        /> : null}
 
-                  {this.props.pageView & 0b1000 ?
-                   <Reservation
-                     title={this.props.selectedMovie}
-                     date={this.props.showtimeDate}
-                     id={this.props.showtimeId}
-                   /> : null}
+      {this.props.pageView & 0b1000 ?
+        <Reservation
+          title={this.props.selectedMovie}
+          date={this.props.showtimeDate}
+          id={this.props.showtimeId}
+          onReservationSubmit={this.props.onReservationSubmit}
+        /> : null}
 
-                </div>
-               );
-    }
+    </div>
+    );
+  }
 
 }
 
 Content.propTypes = {
-    pageView: PropType.number,
-    sits: PropType.object,
-    poster: PropType.string,
+  pageView: PropType.number,
+  sits: PropType.object,
+  poster: PropType.string,
 
-    movies: PropType.array,
-    selectedMovie: PropType.string,
+  city: PropType.string,
+  movies: PropType.array,
+  selectedMovie: PropType.string,
 
-    showtimes: PropType.array,
-    showtimeDate: PropType.string,
-    showtimeId: PropType.string,
+  showtimes: PropType.array,
+  showtimeDate: PropType.string,
+  showtimeId: PropType.string,
 
-    onMovieSelect: PropType.func,
-    onShowtimeSelect: PropType.func,
-    onSitSelect: PropType.func
+  onMovieSelect: PropType.func,
+  onShowtimeSelect: PropType.func,
+  onSitSelect: PropType.func,
+  onReservationSubmit: PropType.func
 };
 
 export default Content;
--- src/components/contentViews/Reservation.jsx
+++ src/components/contentViews/Reservation.jsx
@@ -1,25 +1,36 @@
 import React from 'react';
 import PropType from 'prop-types';
 
+
+
 class Reservation extends React.Component {
+    state = {
+        name: '',
+        email: ''
+    };
+
     onReservationSubmit = (event) => {
         event.preventDefault();
         console.log(`Off we go to external api with id ${this.props.id}`);
+        this.setState({
+        }, () => this.props.onReservationSubmit({ name: this.state.name, email: this.state.email }))
+
     }
 
     render() {
         return (
             <form onSubmit={this.onReservationSubmit}>
-              <h2>Reservation for</h2>
-              <h3>Title: {this.props.title}</h3>
-              <h3>Date: {this.props.date}</h3>
-              Name:<br/>
-              <input type="text" name="name" value="" />
-              <br/>
-              Email:<br/>
-              <input type="text" name="email" value="" />
-              <br></br>
-              <input type="submit" value="Submit" />
+                <h2>Reservation for</h2>
+                <h3>Title: {this.props.title}</h3>
+                <h3>Date: {this.props.date}</h3>
+                <h3>Selected seats: </h3>
+                Name:<br />
+                <input type="text" name="name" value={this.state.name} onChange={e => this.setState({ name: e.target.value })} />
+                <br />
+                Email:<br />
+                <input type="text" name="email" value={this.state.email} onChange={e => this.setState({ email: e.target.value })} />
+                <br></br>
+                <input type="submit" value="Submit" />
             </form>
         );
     }
@@ -30,7 +41,9 @@ Reservation.propTypes = {
     date: PropType.string,
     id: PropType.string,
     row: PropType.number,
-    column: PropType.number
+    column: PropType.number,
+
+    onReservationSubmit: PropType.func
 };
 
 export default Reservation;
